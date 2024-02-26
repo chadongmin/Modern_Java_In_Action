db822840d18de0a8beae713bbca0fef0c4d1b934
chadongmin <cdm2883@naver.com>
2024-02-26 14:33:30 +0900

    함수형 인터페이스에 람다식을 전달할 수 있는 이유

--- src/test/java/FunctionalInterface.java
+++ src/test/java/FunctionalInterface.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FunctionalInterface {
-
     @Test
     public void 함수형인터페이스에_람다식을_전달할수있는이유(){
         Runnable r1 = () -> System.out.println("Hello world");
