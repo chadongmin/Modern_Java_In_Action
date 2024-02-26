b4c8e7f7d47195fc7c695294c13ab83365a66fb1
chadongmin <cdm2883@naver.com>
2024-02-26 16:56:33 +0900

    Function 인터페이스 사용 방법

---
+++ src/test/java/FunctionInterfaceTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FunctionInterfaceTest {
+
+    public <T, R> List<R> map(List<T> list, Function<T,R> f){
+        List<R> result = new ArrayList<>();
+        for (var l : list){
+            result.add(f.apply(l));
+        }
+        return result;
+    }
+    @Test
+    public void function_인터페이스_테스트(){
+
+        List<Integer> StringLenList = new ArrayList<>();
+        List<String> str = List.of("a", "aa", "aaa");
+
+        StringLenList = map(str, (String s) -> s.length());
+
+        assertThat(StringLenList.get(0)).isEqualTo(1);
+        assertThat(StringLenList.get(2)).isEqualTo(3);
+    }
+}
